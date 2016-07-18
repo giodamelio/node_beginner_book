76de6802f5e4af56a8c62322a368e9a9fe96c04e
Gio d'Amelio <giodamelio@gmail.com>
2016-07-18 09:55:27 -0700

    Make the routes hand their own responses

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,13 @@
-module.exports.start = function() {
+module.exports.start = function(response) {
   console.log('Request handler start was called');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello start');
+  response.end();
 }
 
-module.exports.upload = function() {
+module.exports.upload = function(response) {
   console.log('Request handler upload was called');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello upload');
+  response.end();
 }
--- router.js
+++ router.js
@@ -1,9 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log('Routing request at', pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    handle[pathname](response);
   } else {
     console.log('Route', pathname, 'not found');
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('404 Not Found');
+    response.end();
   }
 }
 
