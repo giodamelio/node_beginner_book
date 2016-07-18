738ec485ca1f500e876a2f98b1c11c9fb9187d59
Gio d'Amelio <giodamelio@gmail.com>
2016-07-18 09:40:59 -0700

    Move server into function and create index file

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const url = require('url');
 
-http.createServer(function(request, response) {
-  const pathname = url.parse(request.url).pathname;
-  console.log('Request for', pathname, 'recieved');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
+    console.log('Request for', pathname, 'recieved');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  };
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server started at http://localhost:8888');
+};
+
+exports.start = start;
+
