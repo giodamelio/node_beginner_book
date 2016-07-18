feab617f49f85d1c466750373103aa2cd09e6628
Gio d'Amelio <giodamelio@gmail.com>
2016-07-17 17:41:26 -0700

    Parse path name

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const url = require('url');
 
 http.createServer(function(request, response) {
+  const pathname = url.parse(request.url).pathname;
+  console.log('Request for', pathname, 'recieved');
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello World');
   response.end();
